7c987974599b09fbf2031da510d21570e208adce
Ian Rees <ian@ianrees.net>
2018-09-14 16:25:01 -0700

    Move to transitpbf.h

--- src/mjolnir/valhalla_convert_transit.cc
+++ src/mjolnir/valhalla_convert_transit.cc
@@ -36,6 +36,7 @@
 #include "mjolnir/graphtilebuilder.h"
 #include "mjolnir/servicedays.h"
 #include "mjolnir/validatetransit.h"
+#include "mjolnir/transitpbf.h"
 
 #include <valhalla/proto/transit.pb.h>
 
@@ -43,48 +44,6 @@ using namespace boost::property_tree;
 using namespace valhalla::midgard;
 using namespace valhalla::baldr;
 using namespace valhalla::mjolnir;
-
-// Shape
-struct Shape {
-  uint32_t begins;
-  uint32_t ends;
-  std::vector<PointLL> shape;
-};
-
-struct Departure {
-  GraphId orig_pbf_graphid; // GraphId in pbf tiles
-  GraphId dest_pbf_graphid; // GraphId in pbf tiles
-  uint32_t trip;
-  uint32_t route;
-  uint32_t blockid;
-  uint32_t shapeid;
-  uint32_t headsign_offset;
-  uint32_t dep_time;
-  uint32_t schedule_index;
-  uint32_t frequency_end_time;
-  uint16_t elapsed_time;
-  uint16_t frequency;
-  float orig_dist_traveled;
-  float dest_dist_traveled;
-  bool wheelchair_accessible;
-  bool bicycle_accessible;
-};
-
-// Unique route and stop
-struct TransitLine {
-  uint32_t lineid;
-  uint32_t routeid;
-  GraphId dest_pbf_graphid; // GraphId (from pbf) of the destination stop
-  uint32_t shapeid;
-  float orig_dist_traveled;
-  float dest_dist_traveled;
-};
-
-struct StopEdges {
-  GraphId origin_pbf_graphid;        // GraphId (from pbf) of the origin stop
-  std::vector<GraphId> intrastation; // List of intra-station connections
-  std::vector<TransitLine> lines;    // Set of unique route/stop pairs
-};
 
 // Struct to hold stats information during each threads work
 struct builder_stats {
@@ -98,55 +57,6 @@ struct builder_stats {
     midnight_dep_count += other.midnight_dep_count;
   }
 };
-
-void write_pbf(const Transit& tile, const boost::filesystem::path& transit_tile) {
-  // check for empty stop pairs and routes.
-  if (tile.stop_pairs_size() == 0 && tile.routes_size() == 0 && tile.shapes_size() == 0) {
-    LOG_WARN(transit_tile.string() + " had no data and will not be stored");
-    return;
-  }
-
-  // write pbf to file
-  if (!boost::filesystem::exists(transit_tile.parent_path())) {
-    boost::filesystem::create_directories(transit_tile.parent_path());
-  }
-  auto size = tile.ByteSize();
-  valhalla::midgard::mem_map<char> buffer(transit_tile.string(), size);
-  if (!tile.SerializeToArray(buffer.get(), size)) {
-    LOG_ERROR("Couldn't write: " + transit_tile.string() + " it would have been " +
-              std::to_string(size));
-  }
-
-  if (tile.routes_size() && tile.nodes_size() && tile.stop_pairs_size() && tile.shapes_size()) {
-    LOG_INFO(transit_tile.string() + " had " + std::to_string(tile.nodes_size()) + " nodes " +
-             std::to_string(tile.routes_size()) + " routes " + std::to_string(tile.shapes_size()) +
-             " shapes " + std::to_string(tile.stop_pairs_size()) + " stop pairs");
-  } else {
-    LOG_INFO(transit_tile.string() + " had " + std::to_string(tile.stop_pairs_size()) +
-             " stop pairs");
-  }
-}
-
-Transit read_pbf(const std::string& file_name, std::mutex& lock) {
-  lock.lock();
-  std::fstream file(file_name, std::ios::in | std::ios::binary);
-  if (!file) {
-    throw std::runtime_error("Couldn't load " + file_name);
-    lock.unlock();
-  }
-  std::string buffer((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-  lock.unlock();
-  google::protobuf::io::ArrayInputStream as(static_cast<const void*>(buffer.c_str()), buffer.size());
-  google::protobuf::io::CodedInputStream cs(
-      static_cast<google::protobuf::io::ZeroCopyInputStream*>(&as));
-  auto limit = std::max(static_cast<size_t>(1), buffer.size() * 2);
-  cs.SetTotalBytesLimit(limit, limit);
-  Transit transit;
-  if (!transit.ParseFromCodedStream(&cs)) {
-    throw std::runtime_error("Couldn't load " + file_name);
-  }
-  return transit;
-}
 
 // Get scheduled departures for a stop
 std::unordered_multimap<GraphId, Departure>
@@ -544,18 +454,6 @@ std::list<PointLL> GetShape(const PointLL& stop_ll,
   }
 
   return shape;
-}
-
-// Converts a stop's pbf graph Id to a Valhalla graph Id by adding the
-// tile's node count. Returns an Invalid GraphId if the tile is not found
-// in the list of Valhalla tiles
-GraphId GetGraphId(const GraphId& nodeid, const std::unordered_set<GraphId>& all_tiles) {
-  auto t = all_tiles.find(nodeid.Tile_Base());
-  if (t == all_tiles.end()) {
-    return GraphId(); // Invalid graph Id
-  } else {
-    return {nodeid.tileid(), nodeid.level() + 1, nodeid.id()};
-  }
 }
 
 void AddToGraph(GraphTileBuilder& tilebuilder_transit,
